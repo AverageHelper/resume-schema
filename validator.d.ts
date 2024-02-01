eb31da30da36c47b28a18367257e0e7381901424
AverageHelper <me@average.name>
2024-01-31 22:24:30 -0700

    feat: Add type definitions

---
+++ validator.d.ts
@@ -0,0 +1,11 @@
+import type schemaJson from "./schema.json";
+
+/**
+ * Asynchronously validates a given object against the JsonResume schema.
+ *
+ * @param resumeJson The object to validate.
+ * @param callback A function that is called with the validation result.
+ */
+export function validate(resumeJson: object, callback: (error: unknown, result: { valid: boolean }) => unknown): void;
+
+export const schema: typeof schemaJson;
